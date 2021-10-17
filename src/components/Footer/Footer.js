ee37c838f698a80cfb9ec2686dd7078a43271bfb
Pump <74673280+Pump-Codes@users.noreply.github.com>
2021-10-17 19:16:15 -0400

    PUSH

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://cynathans.com'
+      className='link footer__link'
+    >
+      Created By Pump | Cynathans.com
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,64 @@
+const header = {
+  homepage: 'https://pumpdev.org',
+  title: 'Pump',
+}
+
+const about = {
+
+  name: 'Pump',
+  role: 'Software Engineer',
+  description:
+    'Hello my name is Pump, I am a developer. I have coding for around 2 years, I am currently specialized in web development, bot development, FiveM development, and Minecraft development.',
+  social: {
+    github: 'https://github.com/Pump-codes',
+    discord: 'https://github.com/Pump-codes',
+  },
+}
+
+const projects = [
+
+  {
+    name: 'Cynathans',
+    description:
+      'Cynathans is a multipurpose discord bot. It has over 70 commands and ready for mostly everything.',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    livePreview: 'https://cynathans.com',
+  },
+  {
+    name: 'Suicide Plugin',
+    description:
+      'The suicide plugin runs off a command (/die) when you type that command you die.',
+    stack: ['JAVA'],
+    sourceCode: 'https://github.com/cynathans/Suicide-Plugin',
+  },
+  {
+    name: 'Dumpster Diving Script (QBUS)',
+    description:
+      'FiveM dumpster diving script made for the QBUS framework.',
+    stack: ['LUA'],
+    sourceCode: 'https://github.com/Pump-Codes/pump-dumpsterdiving',
+  },
+]
+
+const skills = [
+
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Java',
+  'Python',
+  'Git',
+  'CentOS',
+  'Ubuntu',
+  'C#',
+  'C++',
+  'LUA',
+]
+
+const contact = {
+  email: 'me@pumpdev.org',
+}
+
+export { header, about, projects, skills, contact }
